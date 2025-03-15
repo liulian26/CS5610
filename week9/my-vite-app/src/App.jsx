c4422b9a2506d4449712632eb9b80995e0d3f225
liulian26 <breadlv326@gmail.com>
2025-03-14 20:10:42 -0700

    Add TaskList component and integrate it into App component

--- week9/my-vite-app/src/App.jsx
+++ week9/my-vite-app/src/App.jsx
@@ -1,36 +1,14 @@
 import React from 'react';
 import Header from './components /Header';
+import TaskList from './components /TasksList';
 
 export default function App() {
-  const tasks = [
-    {
-      id: 1,
-      title: "Review week 9 material",
-      date: "June 4th at 1 pm",
-    },
-    {
-      id: 2,
-      title: "Do quiz 9",
-      date: "June 4th at 6 pm",
-    },
-    {
-      id: 3,
-      title: "Work on assignment 2",
-      date: "June 5th at 8 am",
-    },
-  ];
   const appName = "My Awesome App"
 
   return(
     <div className="appContainer">
       <Header myAppName={appName} version={2} />  
-      <ul>
-        {/* using array.map to render the tasks array titles 
-        for each element return an <li> */}
-        {tasks.map((task) => {
-          return <li key={task.id}>{task.title}</li>;
-        })}
-      </ul>
+      <TaskList />
     </div>
   );
 }
