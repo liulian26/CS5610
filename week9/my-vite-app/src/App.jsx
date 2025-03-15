c214d60b8d989d730bf4e3f602341d3f3a4b07ad
liulian26 <breadlv326@gmail.com>
2025-03-14 19:53:41 -0700

    Create a file named Header.jsx and write a functional component in it

--- week9/my-vite-app/src/App.jsx
+++ week9/my-vite-app/src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+import React from 'react';
+import Header from './components /Header';
 
 export default function App() {
   const appName = "My Awesome App"
 
   return(
     <div>
-      <p>Welcome to {appName}</p>
+      <Header myAppName={appName} version={2} />  
     </div>
   );
 }
