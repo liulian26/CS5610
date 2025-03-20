b96d1cdd24771391cebb15457ccf151a4a9ef774
liulian26 <breadlv326@gmail.com>
2025-03-20 12:07:56 -0700

    Add tasks data to db.json and fetch tasks in TaskList component

--- week9/my-vite-app/src/components /TasksList.jsx
+++ week9/my-vite-app/src/components /TasksList.jsx
@@ -3,23 +3,15 @@ import Task from "./Task";
 
 
 export default function TaskList(){
-    const [tasks, setTasks] = useState([
-        {
-          id: 1,
-          title: "Review week 9 material",
-          date: "June 4th at 1 pm",
-        },
-        {
-          id: 2,
-          title: "Do quiz 9",
-          date: "June 4th at 6 pm",
-        },
-        {
-          id: 3,
-          title: "Work on assignment 2",
-          date: "June 5th at 8 am",
-        },
-      ]);
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+      fetch("http://localhost:5001/tasks")
+        .then((response) => response.json())
+        .then((data) => setTasks(data)) 
+        .catch((error) => console.error("Error fetching tasks:", error));
+    }, []);
+    
       const handleDelete = (taskId) => {
         setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
       };
