e62c1a2da13edc66b77d9b4f858681fbf1499827
liulian26 <breadlv326@gmail.com>
2025-03-20 11:20:38 -0700

    Add Task component with delete icon and update styling for task layout

--- week9/my-vite-app/src/components /Task.jsx
+++ week9/my-vite-app/src/components /Task.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import {IoTrashSharp} from "react-icons/io5";
 
 export default function Task({ taskObj }) {
   return (
     <li>
       <div className="taskContainer">
-        <p>{taskObj.title}</p>
-        <p>{taskObj.date}</p>
+        <div className="taskTileIconContainer">
+          <p>{taskObj.title}</p>
+          <IoTrashSharp/>
+        </div>
+          <p>{taskObj.date}</p>
       </div>
     </li>
   );
