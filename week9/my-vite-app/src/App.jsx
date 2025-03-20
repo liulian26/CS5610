fa66309b17a6fb53b1805b694dabbe0f8e839284
liulian26 <breadlv326@gmail.com>
2025-03-20 12:14:19 -0700

    Fetch tasks from API and manage state in App component

--- week9/my-vite-app/src/App.jsx
+++ week9/my-vite-app/src/App.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components /Header';
 import TaskList from './components /TasksList';
 import AddTask from './components /AddTask';
 
 export default function App() {
-  const appName = "My Awesome App"
+  const appName = "My Awesome App";
+  const [tasks, setTasks] = useState([]); 
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("http://localhost:5001/tasks");
+        const data = await response.json();
+        setTasks(data);
+      } catch (error) {
+        console.error("Error fetching tasks:", error);
+      }
+    }
+    fetchData(); 
+  }, []); 
 
   return(
     <div className="appContainer">
       <Header myAppName={appName} version={2} />  
       <AddTask />
-      <TaskList />
+      <TaskList tasks={task} setTasks={setTasks}/>
     </div>
   );
 }
