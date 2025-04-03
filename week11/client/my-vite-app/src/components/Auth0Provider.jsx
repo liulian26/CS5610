6d842ca3eea42040d0d3455586b17c232def3fbf
liulian26 <breadlv326@gmail.com>
2025-04-03 11:41:48 -0700

    Update Auth0Provider component to use environment variables for domain and client ID

--- week11/client/my-vite-app/src/components/Auth0Provider.jsx
+++ week11/client/my-vite-app/src/components/Auth0Provider.jsx
@@ -2,9 +2,17 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import React from "react";
 
 export default function Auth0ProviderWithHistory({ children }) {
-  console.log(import.meta.env);
   return (
-    <Auth0Provider domain=" " clientId="">
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+      onRedirectCallback={(appState) => {
+        navigate((appState && appState.returnTo) || window.location.pathname);
+      }}
+    >
       {children}
     </Auth0Provider>
   );
