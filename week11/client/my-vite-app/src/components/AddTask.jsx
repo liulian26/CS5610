e9d3154237e25d8764c5726fcc28685de3854249
liulian26 <breadlv326@gmail.com>
2025-04-10 11:34:15 -0700

    Implement task addition with API integration and enhance Auth0 provider configuration

--- week11/client/my-vite-app/src/components/AddTask.jsx
+++ week11/client/my-vite-app/src/components/AddTask.jsx
@@ -1,41 +1,66 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router";;
 
 
 export default function AddTask({onAddTask}) {
     const [title, setTitle] = useState("");
     const [date, setDate] = useState("");
 
-    const handleTitleChange = (event) => {
-        setTitle(event.target.value);
-      };
-    const handleDateChange = (event) => {
-        setDate(event.target.value);
-      };
-    
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        
-        const newTask = {
-            title: title,
-            date: date,
-      };
-      onAddTask(newTask);
-      setTitle("");
+    const navigate = useNavigate();
+    async function submitHandler(e) {
+      //e is the event object
+      e.preventDefault();
+      const newTask = { title: title, date: date };
+      console.log("new task is ", newTask);
       setDate("");
-  };
-
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <div className="form-control">
-        <label>Title</label>
-        <input type="text"value={title} onChange={handleTitleChange} />
-      </div>
-      <div className="form-control">
-        <label>Date</label>
-        <input type="text" value={date} onChange={handleDateChange}/>
-      </div>
-      <button type="submit">Save</button>
-    </form>
-  );
-}
+      setTitle("");
+      try {
+        //send a post request to the server
+        const response = await fetch("http://localhost:5001/api/tasks/", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(newTask),
+        });
+        if (!response.ok) {
+          if (response.status === 401) {
+            console.log("You are not authorized to add a task");
+          }
+          return;
+        }
+        const data = await response.json();
+        console.log(data);
+        if (data.acknowledged) {
+          navigate(`tasks/${data.insertedId}`);
+        }
+        //
+      } catch (err) {
+        console.log("submitHandler", err);
+      }
+    }
+  
+    return (
+      <form onSubmit={submitHandler}>
+        <div className="form-control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={title}
+            onChange={function (e) {
+              setTitle(e.target.value);
+            }}
+          />
+        </div>
+        <div className="form-control">
+          <label>Date</label>
+          <input
+            type="text"
+            value={date}
+            onChange={function (e) {
+              setDate(e.target.value);
+            }}
+          />
+        </div>
+        <button type="submit"> Save </button>
+      </form>
+    );
+  }
--- week11/server/routes/tasks.js
+++ week11/server/routes/tasks.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const axios = require("axios");
 const router = express.Router();
 const { ObjectId } = require("mongodb");
 const { auth } = require('express-oauth2-jwt-bearer');
 const db = require("../db");
+
 
 const checkJWT = auth({
   audience: process.env.AUTH0_AUDIENCE,
@@ -21,34 +23,85 @@ router.get("/", async (req, res) => {
 });
 
 
-router.get("/:taskId", async (req, res) => {
-  try {
-    const taskId = req.params.taskId;
-    const task = await db.findTask({ _id: new ObjectId(taskId) });
-
-    res.json({
-      id: task._id,
-      title: task.task,
-      completed: task.completed,
-      username: task.user,
-      date: task.date,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Task not found" });
-  }
-});
-
-
 router.post("/", checkJWT, async (req, res) => {
   try {
-    console.log("req.body", req.body);
-    await db.addToDB(req.body);
-    res.status(201).json({ message: "Task added to database" });
+    console.log("req.body ", req.body);
+    const result = await db.addToDB(req.body);
+    res.json(result);
   } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Internal Server Error" });
+    console.log("post handler ", err);
   }
 });
+
+router.get("/newtask", (req, res) => {
+    res.render("taskForm");
+  });
+  // this is the handler for /tasks
+  router.get("/", async (req, res) => {
+    try {
+      const data = await db.readAll();
+      console.log(data);
+      res.json(data);
+    } catch (err) {
+      console.log(err);
+    }
+    //   const response = await axios.get(
+    //     "https://jsonplaceholder.typicode.com/todos/"
+    //   );
+    //   res.json(response.data);
+    // } catch (err) {
+    //   console.log(err.message);
+    // }
+    //send a get request to jsonPlaceholder API and consume the promise using .then/.catch
+    //   const promise = axios.get("https://jsonplaceholder.typicode.com/todos/");
+    //   promise
+    //     .then((response) => {
+    //       res.json(response.data);
+    //     })
+    //     .catch((err) => console.log(err.status));
+    //res is responsible to send data/files
+    //   res.send("<h1>List of all tasks</h1>");
+  });
+  
+  router.get("/:taskId", async (req, res) => {
+    try {
+      const data = await db.readOne({ _id: new ObjectId(req.params.taskId) });
+      res.json(data);
+    } catch (err) {
+      console.log(err.message);
+    }
+    //send a get request to jsonPlaceholder API and consume the promise using async/await
+    // try {
+    //   const response = await axios.get(
+    //     `https://jsonplaceholder.typicode.com/todos/${req.params.taskId}`
+    //   );
+    //   const userResponse = await axios.get(
+    //     `https://jsonplaceholder.typicode.com/users/${response.data.userId}`
+    //   );
+    //   res.render("task", {
+    //     id: req.params.taskId,
+    //     title: response.data.title,
+    //     user: userResponse.data.name,
+    //     completed: response.data.completed,
+    //   });
+    // } catch (err) {
+    //   console.log(err);
+    // }
+    //res is responsible to send data/files
+    //   console.log(req.params.taskId);
+    //   res.send(`<p>you are viewing task ${req.params.taskId}</p>`);
+  });
+  router.delete("/:taskId", async (req, res) => {
+    console.log("in router delete ", req.params.taskId);
+    try {
+      const result = await db.deleteOne({ _id: new ObjectId(req.params.taskId) });
+      console.log(result);
+      return res.status(200).json({ message: "Task deleted" }); // ✅ Success response
+  
+      // res.redirect("/api/tasks");
+    } catch (err) {
+      console.log("delete router ", err);
+    }
+  });
 
 module.exports = router;
