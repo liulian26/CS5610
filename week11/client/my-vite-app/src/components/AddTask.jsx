5bbf1d7b14c26deb5857d328fd28325476f659c7
liulian26 <breadlv326@gmail.com>
2025-04-10 11:49:18 -0700

    Refactor AddTask component to streamline task submission logic and improve readability

--- week11/client/my-vite-app/src/components/AddTask.jsx
+++ week11/client/my-vite-app/src/components/AddTask.jsx
@@ -1,66 +1,64 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router";;
+import { useNavigate } from "react-router";
 
-
-export default function AddTask({onAddTask}) {
-    const [title, setTitle] = useState("");
-    const [date, setDate] = useState("");
-
-    const navigate = useNavigate();
-    async function submitHandler(e) {
-      //e is the event object
-      e.preventDefault();
-      const newTask = { title: title, date: date };
-      console.log("new task is ", newTask);
-      setDate("");
-      setTitle("");
-      try {
-        //send a post request to the server
-        const response = await fetch("http://localhost:5001/api/tasks/", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(newTask),
-        });
-        if (!response.ok) {
-          if (response.status === 401) {
-            console.log("You are not authorized to add a task");
-          }
-          return;
+export default function AddTask() {
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const navigate = useNavigate();
+  async function submitHandler(e) {
+    //e is the event object
+    e.preventDefault();
+    const newTask = { title: title, date: date };
+    console.log("new task is ", newTask);
+    setDate("");
+    setTitle("");
+    try {
+      //send a post request to the server
+      const response = await fetch("http://localhost:5001/api/tasks/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newTask),
+      });
+      if (!response.ok) {
+        if (response.status === 401) {
+          console.log("You are not authorized to add a task");
         }
-        const data = await response.json();
-        console.log(data);
-        if (data.acknowledged) {
-          navigate(`tasks/${data.insertedId}`);
-        }
-        //
-      } catch (err) {
-        console.log("submitHandler", err);
+        return;
       }
+      const data = await response.json();
+      console.log(data);
+      if (data.acknowledged) {
+        navigate(`tasks/${data.insertedId}`);
+      }
+      //
+    } catch (err) {
+      console.log("submitHandler", err);
     }
-  
-    return (
-      <form onSubmit={submitHandler}>
-        <div className="form-control">
-          <label>Title</label>
-          <input
-            type="text"
-            value={title}
-            onChange={function (e) {
-              setTitle(e.target.value);
-            }}
-          />
-        </div>
-        <div className="form-control">
-          <label>Date</label>
-          <input
-            type="text"
-            value={date}
-            onChange={function (e) {
-              setDate(e.target.value);
-            }}
-          />
-        </div>
-        <button type="submit"> Save </button>
-      </form>
-    );
   }
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="form-control">
+        <label>Title</label>
+        <input
+          type="text"
+          value={title}
+          onChange={function (e) {
+            setTitle(e.target.value);
+          }}
+        />
+      </div>
+      <div className="form-control">
+        <label>Date</label>
+        <input
+          type="text"
+          value={date}
+          onChange={function (e) {
+            setDate(e.target.value);
+          }}
+        />
+      </div>
+      <button type="submit"> Save </button>
+    </form>
+  );
+}
