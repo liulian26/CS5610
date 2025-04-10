73350f08c064776916dab5f770f7cdc9b00da9e2
liulian26 <breadlv326@gmail.com>
2025-04-10 11:46:08 -0700

    Integrate Profile component and update routing in App; adjust Auth0Provider configuration

--- week11/client/my-vite-app/src/App.jsx
+++ week11/client/my-vite-app/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import TasksList from "./components/TasksList";
 import AddTask from "./components/AddTask";
@@ -7,8 +7,8 @@ import TaskDetails from "./components/TaskDetail";
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
 import { useAuth0 } from "@auth0/auth0-react";
+import Profile from "./components/Profile";
 import ProtectedRoute from "./components/ProtectedRoute";
-
 
 export default function App() {
   const { isAuthenticated, isLoading } = useAuth0();
@@ -17,44 +17,15 @@ export default function App() {
   const toggleShowForm = () => {
     setShowForm(!showForm);
   };
+
   const appName = "My Awesome App";
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    async function fetchData() {
-      try {
-        const response = await fetch("http://localhost:5001/tasks", {
-          signal: controller.signal
-        });
-        if (response.ok) {
-          const data = await response.json();
-          setTasks(data);
-          setLoading(false);
-          console.log("fetchData ", data);
-        } else {
-          throw new Error("fetch failed");
-        }
-      } catch (err) {
-        console.log("fetchData ", err);
-      }
-    }
-
-    fetchData();
-
-    return () => {
-      console.log("cleanup");
-      controller.abort();
-    };
-  }, []);
-
   return isLoading ? (
     <img src="https://cdn.auth0.com/blog/auth0-react-sample/assets/loading.svg" />
   ) : (
     <div className="appContainer">
       <div className="navBar">
         <nav>
-        <NavLink
+          <NavLink
             // className={({ isActive }) => {
             //   if (isActive) {
             //     return "activeLink";
@@ -65,9 +36,8 @@ export default function App() {
             Home
           </NavLink>
           <NavLink to="/tasks">Tasks</NavLink>
+          <NavLink to="/profile">Profile</NavLink>
         </nav>
-      
-
         <div>{isAuthenticated ? <LogoutButton /> : <LoginButton />}</div>
       </div>
       <Routes>
--- week11/client/my-vite-app/src/main.jsx
+++ week11/client/my-vite-app/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter} from 'react-router'
 import './index.css'
 import App from './App.jsx'
+import Auth0ProviderWithHistory from "./components/Auth0Provider.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StrictMode>
-      <App />
+      <Auth0ProviderWithHistory>
+        <App />
+      </Auth0ProviderWithHistory>
     </StrictMode>
   </BrowserRouter>
-)
+);
